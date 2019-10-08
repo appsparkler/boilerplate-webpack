2b8bc157c2c1be459d4b2d6b4a7103e96372f613
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-08 11:24:05 +0530

    working build

--- .webpack/output/common.js
+++ .webpack/output/common.js
@@ -1,8 +1,8 @@
 const paths = require('../../config/paths');
 
 const output = {
-  path: paths.buildDir,
-  filename: '[name].js'
+  // path: paths.buildDir,
+  // filename: '[name].[ext]'
 };
 
 module.exports = output;
