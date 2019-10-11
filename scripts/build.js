da7df2d995f6fcfe17414081f28d2cdea1ca05ff
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-11 07:39:50 +0530

    Added Minimist and trying it out :)

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const parseArgs = require('minimist');
 const webpackConfig = {};
 const compiler = webpack(webpackConfig);
 
@@ -6,6 +7,8 @@ compiler.run((err, stats) => {
   if (err || stats.hasErrors()) return console.error(err, stats);
   else displayStats(stats);
 });
+
+console.dir(parseArgs(process.argv));
 
 function displayStats(stats) {
   console.log(stats.toString({
