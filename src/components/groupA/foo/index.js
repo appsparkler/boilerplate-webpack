0f4c782b973f03682b6283f5b5b5d4e74eca3456
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-12 06:28:41 +0530

    corrections

--- src/components/groupA/foo/index.js
+++ src/components/groupA/foo/index.js
@@ -1,6 +1,6 @@
 import './styles.sass';
 
-document.getElementById('foo').innerHTML = '<h1>I am Foo...sball<h1>';
+document.getElementById('foo').innerHTML = '<h2>I am Foo...sball</h2>';
 
 if (module.hot) {
   module.hot.accept();
