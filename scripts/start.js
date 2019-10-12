09dcd32f887126c7a257a2eb663ef74b504e0fa1
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-12 08:19:49 +0530

    added variables

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,12 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+require('../config/env'); // SETUP environment variables
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('../.webpack/config.development');
 const compiler = webpack(webpackConfig);
 const devServerConfig = getDevServerConfig();
 const devServer = new WebpackDevServer(compiler, devServerConfig);
-const PORT = '8888';
+const PORT = process.env.DEV_SERVER_PORT || 8889;
 
 devServer.listen(PORT, () => console.log(`Starting server on http://localhost:${PORT}`));
 
