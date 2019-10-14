85f02eb768134cd2dc75e0801bc8a0a6666ec57b
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-14 07:51:04 +0530

    updated analytics config

--- .webpack/plugins/analytics.js
+++ .webpack/plugins/analytics.js
@@ -1,14 +1,20 @@
-const plugins = require('./common');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const commonPlugins = require('./common');
 const DotEnv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const paths = require('../../config/paths');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = [
-  ...plugins,
-  new BundleAnalyzerPlugin({
-    analyzerPort: process.env.BRANDS_APP_BUNDLE_ANALYZER_PORT || 8000
-  }),
+  ...commonPlugins,
   new DotEnv({
     path: paths.dotEnv.analytics
+  }),
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: '/[name]/css-clientlib-[contenthash:8]/css/styles.css',
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerPort: process.env.BRANDS_APP_BUNDLE_ANALYZER_PORT || 8000
   })
 ];
