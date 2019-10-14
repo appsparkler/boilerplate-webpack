511ff2f6fec80d4f86fd765817bba42be5b34f27
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-14 07:02:07 +0530

    symantec language

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,22 @@
-setupEnvVariables();
-const webpack = require('webpack');
-const args = require('../config/args');
-const webpackConfig = require('../.webpack/config.production');
-const compiler = webpack(webpackConfig);
+const bus = {};
+setupEnvVariables.call(bus);
+runWebpackCompiler.call(bus);
 
-compiler.run((err, stats) => {
-  if (err || stats.hasErrors()) return console.error(err, stats);
-  else displayStats(stats);
-});
-
+// abstract functions
 /*eslint-disable*/
+function runWebpackCompiler() {
+  const webpack = require('webpack');
+  const webpackConfig = require('../.webpack/config.production');
+  this.compiler = webpack(webpackConfig);
+
+  this.compiler.run((err, stats) => {
+    if (err || stats.hasErrors()) return console.error(err, stats);
+    else displayStats(stats);
+  });
+}
+
 function displayStats(stats) {
+  const args = require('../config/args');
   if(args.stats) console.log(stats.toString({
       colors: true
     }));
