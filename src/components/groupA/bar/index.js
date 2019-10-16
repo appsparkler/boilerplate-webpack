7e4632c3f8ef8309b93d4a93d0f108ef6665ea81
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-16 08:46:13 +0530

    updated brand colors

--- src/components/groupA/bar/index.js
+++ src/components/groupA/bar/index.js
@@ -2,7 +2,7 @@ import './styles';
 import 'bootstrap';
 
 document.getElementById('bar').innerHTML = `
-  <h1 class="text-primary bg-secondary">I am Bar...</h1>
+  <h1 class="text-secondary bg-primary ">I am Bar...</h1>
   <div class="test">
     I am Test...
   </div>
