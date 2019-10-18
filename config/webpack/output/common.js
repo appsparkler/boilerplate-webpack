e0315e932d93831a2d33ac701a7ba67e326b3004
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-18 09:48:08 +0530

    code-formatting

--- config/webpack/output/common.js
+++ config/webpack/output/common.js
@@ -1,7 +1,9 @@
-const paths = require(`${process.env.INIT_CWD}/config/paths`);
+const {INIT_CWD, BRAND} = process.env;
+
+const paths = require(`${INIT_CWD}/config/paths`);
 
 const output = {
-  path: `${paths.buildDir}-${process.env.BRAND || 'DEFAULT'}`,
+  path: `${paths.buildDir}-${BRAND || 'DEFAULT'}`,
   filename: `[name]/js-clientlib-[contenthash:8]/js/[name]-script.js`
 };
 
